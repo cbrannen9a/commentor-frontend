d4229266b62b128b991d1b800d64b7680bfbacd6
Chris Brannen <cbrannen@outlook.com>
2020-06-29 19:56:05 +0100

    Typing it up

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,7 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { FC } from "react";
+import { Link } from "gatsby";
 
-const Header = ({ siteTitle }) => (
+const Header: FC<Props> = ({ siteTitle }) => (
   <header
     style={{
       background: `rebeccapurple`,
@@ -29,14 +28,10 @@ const Header = ({ siteTitle }) => (
       </h1>
     </div>
   </header>
-)
+);
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+interface Props {
+  siteTitle: string;
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default Header;
